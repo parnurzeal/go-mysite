7589d18dd1fbfc938f938db2d6688bc0a8c213d9
ParNurZeal <parnurzeal@gmail.com>
2014-08-19 22:37:03 +0900

    Added homepage

--- main.go
+++ main.go
@@ -10,73 +10,9 @@ import (
 	"text/template"
 )
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html", "layout.html"))
+var templates = template.Must(template.ParseFiles("layout.html"))
 var validPath = regexp.MustCompile("(^/(resume)/([a-zA-Z0-9]+)$)|^/$|^$")
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
 func makeLayout(w http.ResponseWriter, r *http.Request) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	fmt.Println(m)
@@ -84,17 +20,27 @@ func makeLayout(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	} else {
-		if m[0] == "" {
-			renderTemplate2(w, "views/homepage.html")
+		if m[0] == "" || m[0] == "/" {
+			renderTemplate(w, "views/homepage.html")
 		} else {
-			renderTemplate2(w, "views/"+m[0])
+			renderTemplate(w, "views/"+m[1])
 		}
 	}
 }
 
-func renderTemplate2(w http.ResponseWriter, tmpl string) {
-
-	err := templates.ExecuteTemplate(w, "layout.html", nil)
+func renderTemplate(w http.ResponseWriter, filename string) {
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	type content struct {
+		Body string
+	}
+	viewData := content{
+		Body: string(body),
+	}
+	err = templates.ExecuteTemplate(w, "layout.html", viewData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
